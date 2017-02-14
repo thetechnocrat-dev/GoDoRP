3fcbfedef0cc1f9e17cd56ffc58c110be1ca0665
McMenemy <mr_mcmenemy@yahoo.com>
2017-02-13 21:42:21 -0800

    add CRUD route examples

--- api/api.go
+++ api/api.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database"
+	"encoding/json"
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"log"
@@ -9,10 +10,82 @@ import (
 	"os"
 )
 
-func dbTestHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	var lastDorp database.Dorp
-	database.DB.Last(&lastDorp)
-	fmt.Fprintf(w, "Author : %s, Message: %s", lastDorp.Author, lastDorp.Message)
+// CRUD Route Handlers
+func createDorpHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	decoder := json.NewDecoder(r.Body)
+	var newDorp database.Dorp
+	if err := decoder.Decode(&newDorp); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	database.DB.Create(&newDorp)
+	res, err := json.Marshal(newDorp)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.Write(res)
+}
+
+func deleteDorpHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var deletedDorp database.Dorp
+	database.DB.Where("ID = ?", ps.ByName("dorpId")).Delete(&deletedDorp) // write now this returns a blank item not the deleted item
+	res, err := json.Marshal(deletedDorp)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+
+	w.Write(res)
+}
+
+func updateDorpHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	type body struct {
+		Author  string
+		Message string
+	}
+	var updates body
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&updates); err != nil {
+		http.Error(w, err.Error(), 400)
+	}
+
+	var updatedDorp database.Dorp
+	database.DB.Where("ID = ?", ps.ByName("dorpId")).First(&updatedDorp)
+	updatedDorp.Author = updates.Author
+	updatedDorp.Message = updates.Message
+	database.DB.Save(&updatedDorp)
+	res, err := json.Marshal(updatedDorp)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+
+	w.Write(res)
+}
+
+func showDorpHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var dorp database.Dorp
+	database.DB.Where("ID = ?", ps.ByName("dorpId")).First(&dorp)
+	res, err := json.Marshal(dorp)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.Write(res)
+}
+
+func indexDorpHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var dorps []database.Dorp
+	database.DB.Find(&dorps)
+	res, err := json.Marshal(dorps)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.Write(res)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -25,7 +98,11 @@ func main() {
 	// add router and routes
 	router := httprouter.New()
 	router.GET("/", indexHandler)
-	router.GET("/dbTest", dbTestHandler)
+	router.POST("/dorp", createDorpHandler)
+	router.GET("/dorp/:dorpId", showDorpHandler)
+	router.DELETE("/dorp/:dorpId", deleteDorpHandler)
+	router.PUT("/dorp/:dorpId", updateDorpHandler)
+	router.GET("/dorps", indexDorpHandler)
 
 	// add database
 	_, err := database.Init()
